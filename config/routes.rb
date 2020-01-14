221e5de9e274fec70c67c10862490ffca63544df
NodokaAoki <genta91nya@gmail.com>
2020-01-14 18:52:29 +0900

    [Start]5-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'post_images#index' # ログイン認証後、投稿画像一覧画面を表示
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
