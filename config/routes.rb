0bd8f944c3f927f951ef4ceeb862243014081e9c
NodokaAoki <genta91nya@gmail.com>
2020-01-15 18:25:10 +0900

    [Finith]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'post_images#index' # ログイン認証後、投稿画像一覧画面を表示
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do #生成するルーティングを限定
   	resource :favorites, only: [:create, :destroy] #いいね機能
   	resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
